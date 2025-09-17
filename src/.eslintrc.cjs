7d37bda853cc30cab9304e6c9430db4d60058b35
MKKLBRFFLT <mikkelreffelt@gmail.com>
2025-09-17 22:45:57 +0200

    add src as regular tracked files

---
+++ src/.eslintrc.cjs
@@ -0,0 +1,39 @@
+/* .eslintrc.cjs */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // enables eslint-plugin-prettier + extends prettier config
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off', // React 17+ (Vite) doesn't need React in scope
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // put TS-specific rule tweaks here if you like
+      },
+    },
+  ],
+};
